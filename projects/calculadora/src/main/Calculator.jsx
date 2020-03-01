66b0f2a3c73e416cdb84b477c16b6c7487c9d563
Gabriel <gabrielroodriz@gmail.com>
2020-03-01 01:02:06 -0300

    fix(Project) Start Project Calculated

---
+++ projects/calculadora/src/main/Calculator.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+
+export default class Calculator extends Component {
+  render() {
+    return (<div className="calculator"></div>);
+  }
+}
