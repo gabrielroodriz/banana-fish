c3417b5d13074745013fc62aaf3b7831646e6d2b
Gabriel <gabrielroodriz@gmail.com>
2020-03-03 20:16:00 -0300

    Fixing(Calculator): Adicionando numeros no display, atribuindo-os a uma posição no array

--- projects/calculadora/src/main/Calculator.jsx
+++ projects/calculadora/src/main/Calculator.jsx
@@ -4,9 +4,18 @@ import "./Calculator.css";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+};
+
 export default class Calculator extends Component {
+  state = { ...initialState };
   clearMemory() {
-    console.log("limpar");
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
@@ -14,14 +23,30 @@ export default class Calculator extends Component {
   }
 
   addDigit(digit) {
-    console.log(digit);
+    if (digit === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + digit;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (digit !== ".") {
+      const indice = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[indice] = newValue;
+      this.setState({ values });
+    }
   }
   render() {
     const addDigit = digit => this.addDigit(digit);
     const setOperation = operation => this.setOperation(operation);
     return (
       <div className="calculator">
-        <Display value={100} />
+        <Display value={this.state.displayValue} />
         <Button label="AC" click={() => this.clearMemory()} triple />
         <Button label="/" click={setOperation} operation />
         <Button label="7" click={addDigit} />
